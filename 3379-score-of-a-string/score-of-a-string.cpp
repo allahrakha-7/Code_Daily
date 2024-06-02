2d869d25149ccd11ccec5493fd964580b88015be
Allah Rakha <128797491+Bhattisahib710@users.noreply.github.com>
2024-06-02 11:40:54 +0500

    Time: 0 ms (100.00%) | Memory: 7.7 MB (78.98%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -4,7 +4,7 @@ public:
         int score = 0;
 
         for (size_t i = 0; i < s.length()-1; ++i) {
-            score += abs(static_cast<int>(s[i]) - static_cast<int>(s[i+1]));
+            score += abs(int(s[i]) - int(s[i+1]));
         
         }
         return score;
