9c1dae93b34520058a3f7e27cfa512fb53841414
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2025-07-12 01:22:37 +0500

    Time: 4 ms (97.36%) | Memory: 11.9 MB (23.62%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,22 +9,30 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if ( !head || !head->next ) 
-        {
+
+        if (!head || !head->next) {
+            
             return false;
+        
         }
-
+        
+        // Initialize two pointers
+        
         ListNode *slow = head;
-        ListNode *fast = head;
-
-        while ( fast && fast->next )
-        {
-            slow = slow->next;
+        
+        ListNode *fast = head; 
+        
+        // Move pointers until they meet or fast reaches end
+        while (fast && fast->next) {
+        
+            slow = slow->next;  
+        
             fast = fast->next->next;
-
-            if ( slow == fast )
-            {
+            
+            if (slow == fast) {
+        
                 return true;
+        
             }
         }
         return false;
