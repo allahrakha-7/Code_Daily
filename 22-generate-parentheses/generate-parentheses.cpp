7a5a4f9e8efd32c476b1443d810add7ca1717378
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2025-08-19 23:47:05 +0500

    Time: 2 ms (82.32%) | Memory: 10.9 MB (96.80%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,27 +1,52 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
+        
         vector<string> result;
-        string current;
-        int left = 0, right = 0;
+        stack<string> st;
+        st.push("(");
 
-        generateParenthesisHelper(n, left, right, current, result);
 
+        while (!st.empty()) {
+
+            string current = st.top();
+
+            st.pop();
+
+            int open = 0;
+            int close = 0;
+
+            for (char c : current) {
+
+                if (c == '(') {
+
+                    open++;
+
+                } else {
+
+                    close++;
+                }
+            }
+            
+            if (open == n && close == n) {
+
+                result.push_back(current);
+
+                continue;
+            }
+            
+            if (open < n) {
+
+                st.push(current + "(");
+
+            }
+
+            if (close < open) {
+
+                st.push(current + ")");
+
+            }
+        }
         return result;
-    }
-
-    void generateParenthesisHelper(int n, int left, int right, string current, vector<string>& result) {
-        if (left == n && right == n) {
-            result.push_back(current);
-            return;
-        }
-
-        if (left < n) {
-            generateParenthesisHelper(n, left + 1, right, current + '(', result);
-        }
-
-        if (right < left) {
-            generateParenthesisHelper(n, left, right + 1, current + ')', result);
-        }
     }
 };
