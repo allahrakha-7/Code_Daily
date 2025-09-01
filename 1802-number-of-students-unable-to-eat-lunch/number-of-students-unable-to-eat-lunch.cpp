bc3d969f2f4099fced29be6c11190ec408958294
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2025-09-02 02:12:05 +0500

    Time: 0 ms (100.00%) | Memory: 11.7 MB (70.23%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -6,17 +6,25 @@ public:
         int square_count = 0;
 
         for (int student : students) {
+
             if (student == 0) {
+
                 circular_count++;
+
             } else {
+
                 square_count++;
             }
         }
 
         for (int sandwich : sandwiches) {
+            
             if (sandwich == 0) {
+             
                 if (circular_count > 0) {
+                    
                     circular_count--;
+                
                 } else {
 
                     return square_count;
@@ -25,6 +33,7 @@ public:
             } else { 
 
                 if (square_count > 0) {
+             
                     square_count--;
 
                 } else {
