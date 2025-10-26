0b5822bc4ef6fdd452e86d449a07af2039e28217
Allah Rakha <128797491+allahrakha-7@users.noreply.github.com>
2025-10-26 08:35:48 +0500

    Time: 0 ms (100.00%) | Memory: 9.7 MB (74.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+
         if (s.length() != t.length()) {
+        
             return false;
+        
         }
 
         unordered_map<char, int> freq_map;
 
         for (char c : s) {
+        
             freq_map[c]++;
+        
         }
 
         for (char c : t) {
+        
             if (freq_map.find(c) == freq_map.end() || freq_map[c] == 0) {
+        
                 return false;
+        
             }
+        
             freq_map[c]--;
+
         }
         return true;
     }
